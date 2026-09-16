072609cfbc4f4ceed8fbc463329eed2ecc56737c
Myself075 <165383704+Myself075@users.noreply.github.com>
2026-09-16 21:57:47 +0530

    Create MainActivity.java

---
+++ app/app/src/main/app/src/main/java/com/arc/runclub/MainActivity.java
@@ -0,0 +1,43 @@
+package com.arc.runclub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setLoadsImagesAutomatically(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl(
+            "https://sites.google.com/view/arc-runclub/home?authuser=0"
+        );
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
